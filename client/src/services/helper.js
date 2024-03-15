a244e21f90b367903bc9827f4c4536f17955b76e
Harsh patel <154687032+patelharshshaileshbhai@users.noreply.github.com>
2024-03-15 21:15:04 +0530

    Update helper.js

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://study-material-app.onrender.com"
-export const BASE_URL = "study-material-app-ip1h.vercel.app"
+export const BASE_URL = "https://study-material-app.onrender.com"
+// export const BASE_URL = "study-material-app-ip1h.vercel.app"
 
